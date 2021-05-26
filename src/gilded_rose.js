6d51aa3ff4568a69e87c3c8c411efd1267d828dd
anthony Payan <anthony.payan08@outlook.fr>
2021-05-26 21:12:25 +0200

    work

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -1,4 +1,5 @@
 //constructeur des produits
+var item = [];
 class Item {
   constructor(name, sellIn, quality){
     this.name = name;
@@ -11,82 +12,152 @@ class Item {
 //quallity : qualitée de l'article
 
 
-
-
-
-
-
-
-
 class Shop {
   constructor(items=[]){
     this.items = items;
   }
 
+  
+
+
 
 
   updateQuality() {
     for (var i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1;
-          }
-        }
 
-
-
-
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
-
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-          }
-        }
+      if (this.items[i].name == ("Aged Brie")) {
+        item = this.items[i];
+        ChangeBrie(item);
       }
 
-
-
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
+      else if (this.items[i].name == ("Backstage passes to a TAFKAL80ETC concert")) {
+        item = this.items[i];
+        Backstage(item);
       }
 
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1;
-              }
-            }
-          }
-          else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality;
-          }
-        }
-        
-        else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
-          }
-        }
+      else if (this.items[i].name == "Sulfuras") {
+        item = this.items[i];
+        Sulfuras(item);
       }
+
+      else if (this.items[i].name.includes("Conjured")) {
+        item = this.items[i];
+        ConjuredQuality(item);
+      }
+
+          else   {
+            item = this.items[i];
+            NormalChange(item);
+          }
     }
 
     return this.items;
   }
 }
+
+function Sulfuras(item) {   
+  return item;
+}
+
+function ConjuredQuality(item) {
+  item.sellIn -= 1;
+  if (item.sellIn >= 0) {
+      item.quality -= 2;
+    }
+    else {
+      if (item.quality >4 ){
+      item.quality -= 4;
+    }
+    else {
+      item.quality = 0;
+    }
+    }
+  return item;
+}
+
+function NormalChange(item) {
+  item.sellIn -= 1;
+  if (item.sellIn >= 0) {
+    item.quality -= 1;
+  }
+  else {
+    if (item.quality >2 ){
+    item.quality -= 2;
+  }
+  else {
+    item.quality = 0;
+  }
+  }
+return item;
+}
+
+
+function ChangeBrie(item) {
+  item.sellIn -= 1;
+    if (item.sellIn > 10) {
+      if (item.quality < 50){
+      item.quality += 1;
+    }
+    else {
+      item.quality = 50;
+    }
+    }
+    else {
+      if (item.sellIn > 5) {
+        if (item.quality < 48){
+        item.quality += 2;
+        }
+        else {
+          item.quality = 50;
+        }
+      } 
+      else {
+        if (item.quality < 47){
+        item.quality += 3;
+        }
+        else {
+          item.quality = 50;
+        }
+      }
+    }
+}
+
+function Backstage(item) {
+  item.sellIn -= 1;
+  if (item.sellIn >= 0) {
+    
+    if (item.sellIn > 10) {
+      if (item.quality < 50){
+      item.quality += 1;
+    }
+    else {
+      item.quality = 50;
+    }
+    }
+    else {
+      if (item.sellIn > 5) {
+        if (item.quality < 48){
+        item.quality += 2;
+        }
+        else {
+          item.quality = 50;
+        }
+      } 
+      else {
+        if (item.quality < 47){
+        item.quality += 3;
+        }
+        else {
+          item.quality = 50;
+        }
+      }
+    }
+    }
+    else {
+      item.quality = 0;
+    }
+}
+
 module.exports = {
   Item,
   Shop
